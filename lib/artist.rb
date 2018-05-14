8f5bdac1b0ccb167f978d8586fa3722885bb9967
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-14 21:14:56 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,17 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Findable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
@@ -19,13 +22,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   @@artists.count
+  # end
 
   def add_song(song)
     @songs << song
---
+++ lib/concerns/findable.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+module Findable
+  module InstanceMethods
+
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
+
+  end
+end
