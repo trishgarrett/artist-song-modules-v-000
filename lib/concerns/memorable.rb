31da784b103156e7475344c9ba800beba8e20ece
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-21 17:28:03 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -9,13 +9,9 @@ module Memorable
     end
   end
 
-  # module InstanceMethods
-  #   def reset_all
-  #     self.all.clear
-  #   end
-  #
-  #   def count
-  #     self.all.count
-  #   end
-  # end
+  module InstanceMethods
+    def initialize
+
+    end  
+  end  
 end
